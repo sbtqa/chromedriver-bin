a35c47820ec40391636578913c708d00abe1fbab
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-26 10:20:27 +0700

    change version variable name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,5 +8,5 @@ fi
 
 mvn clean deploy --settings .travis/settings.xml -DskipTests=true -Drelease=true -B -U
 
-export version=$(grep -o '<version>[0-9.]*</version>' pom.xml | head -1 | sed 's_<version>__' | sed 's_</version>__')
-echo $version
+export CHROMEDRIVER_VERSION=$(grep -o '<version>[0-9.]*</version>' pom.xml | head -1 | sed 's_<version>__' | sed 's_</version>__')
+echo $CHROMEDRIVER_VERSION
--- .travis/pushback/to-pf2.sh
+++ .travis/pushback/to-pf2.sh
@@ -3,9 +3,9 @@ cd page-factory-2
 git remote rm origin
 git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2.git
 printenv
-echo $version
-sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" plugins/html-plugin/src/test/resources/config/application.properties
-sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" plugins/web-plugin/src/test/resources/config/application.properties
+echo $CHROMEDRIVER_VERSION
+sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $CHROMEDRIVER_VERSION/" plugins/html-plugin/src/test/resources/config/application.properties
+sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $CHROMEDRIVER_VERSION/" plugins/web-plugin/src/test/resources/config/application.properties
 git add -A
 git commit -m "bump chrome driver version by chromedriver-bin"
 git pull origin master

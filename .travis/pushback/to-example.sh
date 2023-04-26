1ac8e924cf1bfac45b0b203075dd0250698481bd
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-26 11:01:00 +0700

    fix chromedriver version variable name [ci skip]

--- .travis/pushback/to-example.sh
+++ .travis/pushback/to-example.sh
@@ -2,7 +2,7 @@ git clone https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
 cd page-factory-2-example
 git remote rm origin
 git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
-sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" src/test/resources/config/application.properties
+sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $CHROMEDRIVER_VERSION/" src/test/resources/config/application.properties
 git add -A
 git commit -m "bump chrome driver version by chromedriver-bin"
 git pull origin master

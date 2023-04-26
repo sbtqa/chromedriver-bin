1c6c125843502f6769cabc40735c0a7adbb47ecd
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-26 10:14:22 +0700

    test variables

--- .travis/pushback/to-pf2.sh
+++ .travis/pushback/to-pf2.sh
@@ -2,9 +2,11 @@ git clone https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2.git
 cd page-factory-2
 git remote rm origin
 git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2.git
+printenv
+echo $version
 sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" plugins/html-plugin/src/test/resources/config/application.properties
 sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" plugins/web-plugin/src/test/resources/config/application.properties
 git add -A
 git commit -m "bump chrome driver version by chromedriver-bin"
 git pull origin master
-git push origin master
+#git push origin master

4b987ca227cc53b6076e20220e09934d65329b6c
Konstantin Maltsev <maltsevky@gmail.com>
2023-04-26 09:27:11 +0700

    fix webdriver version

--- .travis/pushback/to-example.sh
+++ .travis/pushback/to-example.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-git clone https://$GITHUB_TOKEN@github.com/sbtqa/chromedriver-bin.git
-cd chromedriver-bin
+git clone https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
+cd page-factory-2-example
 git remote rm origin
-git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/chromedriver-bin.git
-sed -i 's/webdriver.version = [0-9.]*/webdriver.version = $version/' src/test/resources/config/application.properties
+git remote add origin https://$GITHUB_TOKEN@github.com/sbtqa/page-factory-2-example.git
+sed -i "s/webdriver.version = [0-9.]*/webdriver.version = $version/" src/test/resources/config/application.properties
 git add -A
 git commit -m "bump chrome driver version by chromedriver-bin"
 git pull origin master
